192e5784f15284516800451cb1610188ec7d92d4
pikesley <github@orgraphone.org>
2014-12-08 21:44:47 +0000

    Bamboo headphones ON

--- lib/marvel_explorer/explorer.rb
+++ lib/marvel_explorer/explorer.rb
@@ -3,7 +3,7 @@ module MarvelExplorer
     attr_accessor :config
 
     def initialize config_file = "#{ENV['HOME']}/.marvel_explorer/config.yml"
-      @config = YAML.load File.open 'config/defaults.yml'
+      @config = YAML.load(File.open(File.join(File.dirname(__FILE__), '..', '..', 'config/defaults.yml')))
       @config.merge! YAML.load File.open config_file
     end
 
