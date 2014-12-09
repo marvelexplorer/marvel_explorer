80de97897e88a6150e3852bfd553ff58565d07cc
pikesley <github@orgraphone.org>
2014-12-09 15:15:22 +0000

    Better tweet

--- lib/marvel_explorer/version.rb
+++ lib/marvel_explorer/version.rb
@@ -1,3 +1,3 @@
 module MarvelExplorer
-  VERSION = '0.1.8'
+  VERSION = '0.1.9'
 end
--- spec/explorer_spec.rb
+++ spec/explorer_spec.rb
@@ -74,7 +74,7 @@ module MarvelExplorer
       .to_return(status: 200, body: File.read('spec/fixtures/hulk_comics.json'))
       stub_request(:get, /gateway.marvel.com\/v1\/public\/comics/)
       .to_return(status: 200, body: File.read('spec/fixtures/double-shot-characters.json'))
-      expect(@me.tweet_message).to eq 'In 2003, Hulk appeared in issue #2 of the 2003 run of Marvel Double Shot with Avengers'
+      expect(@me.tweet_message).to eq 'In 2003, Hulk appeared in Marvel Double Shot #2 with Avengers'
     end
 
     it 'should generate a commit message' do
